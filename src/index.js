997471e95df8ec00dfd513adc271dda60431310e
Philip W <philip.whitt@sbcglobal.net>
2019-03-09 10:52:53 -0600

    Removing router/route so gh-pages will work

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { Route } from 'react-router-dom'
 import configureStore from './store/configureStore'
 import Header from './components/Header'
 import UserList from './components/UserList'
@@ -11,13 +9,11 @@ import './styles.scss';
 const store = configureStore()
 
 render(
-  <Router>
-    <Provider store={store}>
-      <div>
-        <Header />
-        <Route path="/" exact component={UserList} />
-      </div>
-    </Provider>
-  </Router>,
+  <Provider store={store}>
+    <div>
+      <Header />
+      <UserList />
+    </div>
+  </Provider>,
   document.getElementById('root')
 )
