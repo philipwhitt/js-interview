01136a4c860d4bae27bcd5141dc7a9e78af98eb4
Philip W <philip.whitt@sbcglobal.net>
2019-03-09 10:51:29 -0600

    Updates

---
+++ precache-manifest.09093e4e09a9ffdebb5393129735d260.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/js-interview/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "59ea537186e4cb59039d",
+    "url": "/js-interview/static/css/main.f305781c.chunk.css"
+  },
+  {
+    "revision": "adedb15082d513c78ccd",
+    "url": "/js-interview/static/js/runtime~main.55645371.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/js-interview/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "7dfd2df592101b936002",
+    "url": "/js-interview/static/js/2.38cc7921.chunk.js"
+  },
+  {
+    "revision": "59ea537186e4cb59039d",
+    "url": "/js-interview/static/js/main.8f7dbbfe.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/js-interview/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/js-interview/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/js-interview/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "7dfd2df592101b936002",
+    "url": "/js-interview/static/css/2.3ac88385.chunk.css"
+  },
+  {
+    "revision": "c73c444018f714c00cbfddaea88e9975",
+    "url": "/js-interview/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.adf663fab9caf83bd4283a3e67e1d25c.js"
+  "/js-interview/precache-manifest.09093e4e09a9ffdebb5393129735d260.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/js-interview/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
