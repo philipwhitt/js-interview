e675d7e4cf00cded7804e4a6174e315525538c55
Philip W <philip.whitt@sbcglobal.net>
2019-03-09 10:26:14 -0600

    Adding prod build assets

---
+++ build/precache-manifest.6eb4b0043b5149ca6f67782b45c10343.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "797b8659cb230e4338e0",
+    "url": "/static/css/main.f305781c.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "0da975dbdb214fca0b89",
+    "url": "/static/js/2.38cc7921.chunk.js"
+  },
+  {
+    "revision": "797b8659cb230e4338e0",
+    "url": "/static/js/main.2ed3c181.chunk.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "0da975dbdb214fca0b89",
+    "url": "/static/css/2.303e54b9.chunk.css"
+  },
+  {
+    "revision": "f9a8a5365c053bec3473f7ea5748df57",
+    "url": "/index.html"
+  }
+];
